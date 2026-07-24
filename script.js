e37617dd2db64f77fdfa5fc024fe1e533b26978b
SHGT <shglobaltechnology1981@gmail.com>
2026-07-24 12:38:41 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*==========================================
+ /*==========================================
 SH GLOBAL TECHNOLOGY
 JavaScript File
 ==========================================*/
@@ -209,7 +209,39 @@ footer.innerHTML =
 " SH Global Technology<br>All Rights Reserved.";
 
 }
+// =================== AUTO GALLERY ===================
 
+const galleryContainer = document.getElementById("gallery-container");
+
+if (galleryContainer) {
+
+fetch("products.json")
+
+.then(response => response.json())
+
+.then(products => {
+
+products.forEach(product => {
+
+galleryContainer.innerHTML += `
+
+<div class="product-card">
+
+<img src="${product.image}" alt="${product.name}">
+
+<h3>${product.name}</h3>
+
+<p>${product.description}</p>
+
+</div>
+
+`;
+
+});
+
+});
+
+}
 /*==========================================
 CONSOLE MESSAGE
 ==========================================*/
